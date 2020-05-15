72946930c7354e1f964d4cfee91a2e5005c0b2bb
root <13264032369@163.com>
2020-05-16 02:35:53 +0800

    New file

---
+++ set_idrac_ntp.py
@@ -0,0 +1,113 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# By Zack_Hou, 2020
+
+import os, sys
+import subprocess
+import time
+from threading import Timer
+import requests
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0]))))
+
+def run_cmd(cmd, stdout="true", time_out=120):
+    """
+    Execute shell command.
+    """
+    result = ""
+    pro = subprocess.Popen(cmd, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+    timer = Timer(time_out, lambda process: process.kill(), [pro])
+    try:
+        timer.start()
+        result = pro.stdout.read()
+        if stdout == "true":
+            return result
+    finally:
+        timer.cancel()
+
+#下载racadm安装包
+def get_install_racadm_set():
+
+    os_version_6 = run_cmd("cat /etc/redhat-release |grep 'release 6'", 'true')
+    os_version_7 = run_cmd("cat /etc/redhat-release |grep 'release 7'", 'true')
+    os_version_8 = run_cmd("cat /etc/redhat-release |grep 'release 8'", 'true')
+
+    if os_version_6:
+        r = requests.get("https://dl.dell.com/FOLDER05446180M/1/DellEMC-iDRACTools-Web-LX-9.3.0-3379_A00.tar.gz")
+
+        if r.status_code == 200:
+            with open('DellEMC-iDRACTools-Web-LX-9.3.0-3379_A00.tar.gz','wb') as code:
+                code.write(r.content)
+            run_cmd('tar -zxf DellEMC-iDRACTools-Web-LX-9.3.0-3379_A00.tar.gz', 'true')
+            run_cmd('rpm -ivh ./iDRACTools/racadm/RHEL6/x86_64/srvadmin-hapi-9.3.0-3379.14516.el6.x86_64.rpm', 'true')
+            run_cmd('rpm -ivh ./iDRACTools/racadm/RHEL6/x86_64/srvadmin-argtable2-9.3.0-3379.14516.el6.x86_64.rpm', 'true')
+            run_cmd('rpm -ivh ./iDRACTools/racadm/RHEL6/x86_64/srvadmin-idracadm7-9.3.0-3379.14516.el6.x86_64.rpm', 'true')
+            #设置iDrac时区为上海
+            run_cmd('/opt/dell/srvadmin/sbin/racadm set iDRAC.Time.Timezone Asia/Shanghai', 'true')
+            time.sleep(3)
+            #启用iDrac的NTP（1启用0关闭）
+            run_cmd('/opt/dell/srvadmin/sbin/racadm set iDRAC.NTPConfigGroup.NTPEnable 1', 'true')
+            #设置iDrac NTP服务器1,此处设置的是网上搜的一个阿里的NTP服务器ip，如果需要配置域名NTP地址例如ntp1.aliyun.com，则还需要配置iDrac的dns。
+            run_cmd('/opt/dell/srvadmin/sbin/racadm set iDRAC.NTPConfigGroup.NTP1 120.25.115.20]', 'true')
+            # 设置iDrac NTP服务器2此处设置的是网上搜的一个腾讯的NTP服务器ip，如果需要配置域名NTP地址例如time1.cloud.tencent.com，则还需要配置iDrac的dns。
+            run_cmd('/opt/dell/srvadmin/sbin/racadm set iDRAC.NTPConfigGroup.NTP2 139.199.215.251', 'true')
+        else:
+            return "CentOS 6 下载iDRACTools-9.3.0失败。"
+    elif os_version_7:
+        r = requests.get("https://dl.dell.com/FOLDER05920767M/1/DellEMC-iDRACTools-Web-LX-9.4.0-3732_A00.tar.gz")
+
+        if r.status_code == 200:
+            with open('DellEMC-iDRACTools-Web-LX-9.4.0-3732_A00.tar.gz','wb') as code:
+                code.write(r.content)
+            run_cmd('tar -zxf DellEMC-iDRACTools-Web-LX-9.4.0-3732_A00.tar.gz', 'true')
+            run_cmd('rpm -ivh ./iDRACTools/racadm/RHEL7/x86_64/srvadmin-hapi-9.3.0-3379.14516.el6.x86_64.rpm', 'true')
+            run_cmd('rpm -ivh ./iDRACTools/racadm/RHEL7/x86_64/srvadmin-argtable2-9.3.0-3379.14516.el6.x86_64.rpm', 'true')
+            run_cmd('rpm -ivh ./iDRACTools/racadm/RHEL7/x86_64/srvadmin-idracadm7-9.3.0-3379.14516.el6.x86_64.rpm', 'true')
+            #设置iDrac时区为上海
+            run_cmd('/opt/dell/srvadmin/sbin/racadm set iDRAC.Time.Timezone Asia/Shanghai', 'true')
+            time.sleep(3)
+            #启用iDrac的NTP（1启用0关闭）
+            run_cmd('/opt/dell/srvadmin/sbin/racadm set iDRAC.NTPConfigGroup.NTPEnable 1', 'true')
+            # 设置iDrac NTP服务器1,此处设置的是网上搜的一个阿里的NTP服务器ip，如果需要配置域名NTP地址例如ntp1.aliyun.com，则还需要配置iDrac的dns。
+            run_cmd('/opt/dell/srvadmin/sbin/racadm set iDRAC.NTPConfigGroup.NTP1 120.25.115.20]', 'true')
+            # 设置iDrac NTP服务器2此处设置的是网上搜的一个腾讯的NTP服务器ip，如果需要配置域名NTP地址例如time1.cloud.tencent.com，则还需要配置iDrac的dns。
+            run_cmd('/opt/dell/srvadmin/sbin/racadm set iDRAC.NTPConfigGroup.NTP2 139.199.215.251', 'true')
+        else:
+            return "CentOS 7 下载iDRACTools-9.4.0失败。"
+    elif os_version_8:
+        elif os_version_7:
+        r = requests.get("https://dl.dell.com/FOLDER05920767M/1/DellEMC-iDRACTools-Web-LX-9.4.0-3732_A00.tar.gz")
+
+        if r.status_code == 200:
+            with open('DellEMC-iDRACTools-Web-LX-9.4.0-3732_A00.tar.gz', 'wb') as code:
+                code.write(r.content)
+            run_cmd('tar -zxf DellEMC-iDRACTools-Web-LX-9.4.0-3732_A00.tar.gz', 'true')
+            run_cmd('rpm -ivh ./iDRACTools/racadm/RHEL8/x86_64/srvadmin-hapi-9.3.0-3379.14516.el6.x86_64.rpm', 'true')
+            run_cmd('rpm -ivh ./iDRACTools/racadm/RHEL8/x86_64/srvadmin-argtable2-9.3.0-3379.14516.el6.x86_64.rpm',
+                    'true')
+            run_cmd('rpm -ivh ./iDRACTools/racadm/RHEL8/x86_64/srvadmin-idracadm7-9.3.0-3379.14516.el6.x86_64.rpm',
+                    'true')
+            # 设置iDrac时区为上海
+            run_cmd('/opt/dell/srvadmin/sbin/racadm set iDRAC.Time.Timezone Asia/Shanghai', 'true')
+            time.sleep(3)
+            # 启用iDrac的NTP（1启用0关闭）
+            run_cmd('/opt/dell/srvadmin/sbin/racadm set iDRAC.NTPConfigGroup.NTPEnable 1', 'true')
+            # 设置iDrac NTP服务器1,此处设置的是网上搜的一个阿里的NTP服务器ip，如果需要配置域名NTP地址例如ntp1.aliyun.com，则还需要配置iDrac的dns。
+            run_cmd('/opt/dell/srvadmin/sbin/racadm set iDRAC.NTPConfigGroup.NTP1 120.25.115.20]', 'true')
+            # 设置iDrac NTP服务器2此处设置的是网上搜的一个腾讯的NTP服务器ip，如果需要配置域名NTP地址例如time1.cloud.tencent.com，则还需要配置iDrac的dns。
+            run_cmd('/opt/dell/srvadmin/sbin/racadm set iDRAC.NTPConfigGroup.NTP2 139.199.215.251', 'true')
+        else:
+            return "CentOS 8 下载iDRACTools-9.4.0失败。"
+
+
+#判断机型
+# def if_model():
+#     dell_model = run_cmd("dmidecode -t 1|grep 'Dell'", 'true')
+#     if dell_model:
+#         get_racadm()
+#         logger.send(0, "set dell idrac ntp")
+#     else:
+#          logger.send(1, "It's not a dell server")
+
+if __name__ == '__main__':
+    get_install_racadm_set()
